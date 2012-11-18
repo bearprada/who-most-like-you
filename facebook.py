b26bd2980f5f22b93d9a0d1deaa06b3238d06cf8
prada hsiung <bear.prada@gmail.com>
2012-11-18 17:15:22 +0800

    add url encode for query params

--- facebook.py
+++ facebook.py
@@ -68,10 +68,13 @@ class FqlReporterHandler(BaseHandler, tornado.auth.FacebookGraphMixin):
     @tornado.web.authenticated
     @tornado.web.asynchronous
     def get(self):
-        query = tornado.escape.url_escape('{"post_ids":"SELECT post_id FROM stream WHERE source_id=me() AND likes.count>0 LIMIT 5000",' + \
-                '"like_ids":"SELECT name,sex FROM user WHERE uid IN (SELECT user_id FROM like WHERE post_id IN (SELECT post_id FROM #post_ids))"}')
-        self.facebook_request("/fql?q="+query, self._handle_result,
-                              access_token=self.current_user["access_token"])
+        #query = tornado.escape.url_escape('{"post_ids":"SELECT post_id FROM stream WHERE source_id=me() AND likes.count>0 LIMIT 5000",' + \
+        #        '"like_ids":"SELECT name,sex FROM user WHERE uid IN (SELECT user_id FROM like WHERE post_id IN (SELECT post_id FROM #post_ids))"}')
+        query = '{"post_ids":"SELECT post_id FROM stream WHERE source_id=me() AND likes.count>0 LIMIT 5000",' + \
+                '"like_ids":"SELECT name,sex FROM user WHERE uid IN (SELECT user_id FROM like WHERE post_id IN (SELECT post_id FROM #post_ids))"}'
+        self.facebook_request("/fql"+, self._handle_result,
+                              access_token=self.current_user["access_token"],
+                              q=query)
         self.o = {'name':'likes' , 'children':[ {'name':'female','children':[]} ,  {'name':'male','children':[]}  ]}
         self.set_header('Content-Type', 'application/json')
 
