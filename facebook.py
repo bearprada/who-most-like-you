bfea1299145389b902ebefe1ba23419471895511
prada hsiung <bear.prada@gmail.com>
2012-11-18 17:31:48 +0800

    add the count limitation …. should over 5 likes

--- facebook.py
+++ facebook.py
@@ -98,9 +98,11 @@ class FqlReporterHandler(BaseHandler, tornado.auth.FacebookGraphMixin):
                         m_r[fid] = 1
 
                 for k in m_r:
-                    self.o['children'][1]['children'].append({'name':k , 'size':m_r[k]})
+                    if m_r[k]>5:
+                        self.o['children'][1]['children'].append({'name':k , 'size':m_r[k]})
                 for k in f_r:
-                    self.o['children'][0]['children'].append({'name':k , 'size':f_r[k]})
+                    if f_r[k]>5:
+                        self.o['children'][0]['children'].append({'name':k , 'size':f_r[k]})
             self._output()
 
     def _output(self):
